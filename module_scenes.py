6b6f5c7a05d2cdca3362ebef6ede3f0988df7099
KissMyAxe <kissmyaxe@live.com>
2013-04-30 16:33:01 -0400

    Added updated Liberty, Sota's maps and re-added Snoop's Exile

--- module_scenes.py
+++ module_scenes.py
@@ -13,10 +13,20 @@
 ("multi_scene_pmp_hills",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x0000000130028c8a000641900000494d0000cb4f0000fb5a",
     [],[], "outer_terrain_plain"),
 ("multi_scene_pmp_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000005300002840005795e0000bf0b000070a20000d9ae",
-    [],[], "outer_terrain_plain"),
+    [],[], "outer_terrain_plain"), 
 ("multi_scene_pmp_delta",sf_generate,"none", "none", (0,0), (240,240),-0.5,"0x0000000130000285000641900000793b0000104d0000fb64",
-    [],[],"outer_terrain_beach"),
+    [],[],"outer_terrain_beach"), # Delta by Vendigr
 ("multi_scene_pmp_oasis",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001d00a9cba000651940000cf3f0000818300001f20",
-    [],[],"outer_terrain_desert"),
+    [],[],"outer_terrain_desert"), # Oasis by Fehnor
 ("multi_scene_liberty",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000b0000500000350d600005d4600003c3100006cf6",
-    [],[], "outer_terrain_beach"),
+    [],[], "outer_terrain_beach"), # Liberty by Romans
+("multi_scene_snoops_exile_island",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300005000006f5bd00004ff5000004000000631b",
+    [],[], "outer_terrain_plain"), # Snoop's Exile Island by Slytacular and Zaffa
+("multi_scene_bestopen",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300416a600035cd600007ee80000012100003fbc",
+    [],[], "outer_terrain_plain"), # BestOpenMap by Sotamursu
+("multi_scene_haranaer",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000400032320003c0f300001f9e000011180000031c",
+    [],[], "outer_terrain_desert"), # Haranaer by Sotamursu
+("multi_scene_wold",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000012002a0b20004992700006e54000007fe00001fd2",
+    [],[], "outer_terrain_steppe"), # The Wold by Sotamursu
+("multi_scene_urabhaya",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000005000d795000350d4000011a4000017ee0000440d",
+    [],[], "outer_terrain_plain"), # Urabhaya by Sotamursu
