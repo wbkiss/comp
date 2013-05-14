4aaf7e0dad8ae228f25d04e57690aec65f257de2
KissMyAxe <kissmyaxe@live.com>
2013-05-13 20:00:57 -0400

    Removed old maps, renamed some, sorted maps by status in module_scenes

--- module_scenes.py
+++ module_scenes.py
@@ -1,34 +1,34 @@
-("multi_scene_pmp_bay_farm",sf_generate,"none", "none", (0,0), (100,100),-100,"0x000000013003d7e30005053f00003b4e0000146300006e84",
-    [],[],"outer_terrain_beach"),
-("multi_scene_pmp_suno_valley",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x00000005300014000006419000008a34000029200000031a",
-    [],[], "outer_terrain_plain"),
-("multi_scene_pmp_river_island",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x00000001394018dd000649920004406900002920000056d7",
-    [],[], "outer_terrain_plain"),
-("multi_scene_pmp_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000001394018dd000649920004406900002920000056d7",
-    [],[], "outer_terrain_plain"),
-("multi_scene_pmp_open_plain",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x0000000130000000000641900000bf4a00001d670000d869",
-    [],[], "outer_terrain_plain"),
-("multi_scene_pmp_riverworld",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x0000000130000280800641900000d66e0000e75200005b26",
-    [],[], "outer_terrain_plain"),
-("multi_scene_pmp_hills",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x0000000130028c8a000641900000494d0000cb4f0000fb5a",
-    [],[], "outer_terrain_plain"),
-("multi_scene_pmp_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000005300002840005795e0000bf0b000070a20000d9ae",
-    [],[], "outer_terrain_plain"), 
-("multi_scene_pmp_delta",sf_generate,"none", "none", (0,0), (240,240),-0.5,"0x0000000130000285000641900000793b0000104d0000fb64",
-    [],[],"outer_terrain_beach"), # Delta by Vendigr
-("multi_scene_pmp_oasis",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001d00a9cba000651940000cf3f0000818300001f20",
-    [],[],"outer_terrain_desert"), # Oasis by Fehnor
+# Stable
+("multi_scene_solace",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
+    [],[],"outer_terrain_plain"), # Solace (mixed) by Romans
+("multi_scene_dry_valley",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000450000500000591660000431900001af3000077e3",
+    [],[],"outer_terrain_desert"), # Dry Valley (mixed) by Lord Rich (AZAN)
+("multi_scene_verloren",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001300389800003a4ea000058340000637a0000399b",
+    [],[],"outer_terrain_plain"),
+
+# Testing
+
+("multi_scene_open_plain",sf_generate,"none", "none", (0,0),(240,240),-0.5,"0x0000000130000000000641900000bf4a00001d670000d869",
+    [],[], "outer_terrain_plain"), # Open Plain (open) by KissMyAxe
+("multi_scene_oasis",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000001d00a9cba000651940000cf3f0000818300001f20",
+    [],[],"outer_terrain_desert"), # Oasis (open) by Fehnor
+	
+# Unstable
+("multi_scene_swamp",sf_generate|sf_muddy_water,"none", "none", (0,0),(240,240), 0.0,"0x00000001394018dd000649920004406900002920000056d7",
+    [],[], "outer_terrain_plain"), # Swamp (open/mixed) by KissMyAxe
+("multi_scene_delta",sf_generate,"none", "none", (0,0), (240,240),-0.5,"0x0000000130000285000641900000793b0000104d0000fb64",
+    [],[],"outer_terrain_beach"), # Delta (open) by Vendigr
 ("multi_scene_liberty",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000b0000500000350d600005d4600003c3100006cf6",
-    [],[], "outer_terrain_beach"), # Liberty by Romans
+    [],[], "outer_terrain_beach"), # Liberty (mixed/closed) by Romans
 ("multi_scene_snoops_exile_island",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300005000006f5bd00004ff5000004000000631b",
-    [],[], "outer_terrain_plain"), # Snoop's Exile Island by Slytacular and Zaffa
+    [],[], "outer_terrain_plain"), # Snoop's Exile Island (mixed) by Slytacular and Zaffa
 ("multi_scene_bestopen",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000300416a600035cd600007ee80000012100003fbc",
-    [],[], "outer_terrain_plain"), # BestOpenMap by Sotamursu
+    [],[], "outer_terrain_plain"), # BestOpenMap (open) by Sotamursu
 ("multi_scene_haranaer",sf_generate,"none", "none", (0,0),(100,100),-100,"0x00000000400032320003c0f300001f9e000011180000031c",
-    [],[], "outer_terrain_desert"), # Haranaer by Sotamursu
+    [],[], "outer_terrain_desert"), # Haranaer (open) by Sotamursu
 ("multi_scene_wold",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000012002a0b20004992700006e54000007fe00001fd2",
-    [],[], "outer_terrain_steppe"), # The Wold by Sotamursu
+    [],[], "outer_terrain_steppe"), # The Wold (open) by Sotamursu
 ("multi_scene_urabhaya",sf_generate,"none", "none", (0,0),(100,100),-100,"0x000000005000d795000350d4000011a4000017ee0000440d",
-    [],[], "outer_terrain_plain"), # Urabhaya by Sotamursu
+    [],[], "outer_terrain_plain"), # Urabhaya (closed) by Sotamursu
 ("multi_scene_rock_port",sf_generate,"none", "none", (0,0),(100,100),-100,"0x0000000032224e0a800435130000794a0000305500001978",
-    [],[], "outer_terrain_plain"), # Rock Port by Romans
+    [],[], "outer_terrain_plain"), # Rock Port (mixed) by Romans
